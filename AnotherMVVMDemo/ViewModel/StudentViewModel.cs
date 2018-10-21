11cae2568335dd5e0ef4d11afe343c151b6cc231
kurskiev <timur.k@caspel.com>
2018-10-21 21:06:11 +0300

    Add project files.

---
+++ AnotherMVVMDemo/ViewModel/StudentViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.ObjectModel;
+
+namespace AnotherMVVMDemo
+{
+    public class StudentViewModel
+    {
+        public StudentViewModel ()
+        {
+            LoadStudents();
+        }
+        public ObservableCollection<Student> Students { get; set; }
+
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+
+            students.Add(new Student { FirstName = "Andrey", LastName = "Petrov" });
+            students.Add(new Student { FirstName = "Volodya", LastName = "Vasilyev" });
+            students.Add(new Student { FirstName = "Stepan", LastName = "Razin"});
+
+            Students = students;
+        }
+    }
+}

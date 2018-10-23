82bb3d00b7332e16e3c9fa149aa2eecc8679da71
kurskiev <timur.k@caspel.com>
2018-10-23 12:26:11 +0300

    Add project files.

---
+++ AnotherMVVMDemo/ViewModel/StudentViewModel.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.ObjectModel;
+
+namespace AnotherMVVMDemo
+{
+    public class StudentViewModel
+    {
+        public MyICommand DeleteCommand { get; set; }
+        public MyICommand AddCommand { get; set; }
+
+        private Student _selectedStudent;
+        public Student SelectedStudent
+        {
+            get
+            {
+                return _selectedStudent;
+            }
+            set
+            {
+                _selectedStudent = value;
+                DeleteCommand.RaiseCanExecuteChanged();
+            }
+        }
+        public StudentViewModel ()
+        {
+            LoadStudents();
+            DeleteCommand = new MyICommand(OnDelete, CanDelete);
+            AddCommand = new MyICommand(OnAdd);
+        }
+        public ObservableCollection<Student> Students { get; set; }
+
+        public void LoadStudents()
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+
+            students.Add(new Student { FirstName = "Andrey", LastName = "Petrov" });
+            students.Add(new Student { FirstName = "Volodya", LastName = "Vasilyev" });
+            students.Add(new Student { FirstName = "Stepan", LastName = "Razin"});
+
+            Students = students;
+        }
+
+        private void OnDelete()
+        {
+            Students.Remove(SelectedStudent);
+        }
+
+        private bool CanDelete()
+        {
+            return SelectedStudent != null;
+        }
+
+        private void OnAdd()
+        {
+            Students.Add(new Student { FirstName = "", LastName = "" });
+            //AddCommand.RaiseCanExecuteChanged();
+        }
+    }
+}
---
+++ AnotherMVVMDemo/Views/StudentVIew.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnotherMVVMDemo.Views
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class StudentView : UserControl
+    {
+        public StudentView()
+        {
+            InitializeComponent();
+
+            // View first construction in code-behind
+            //this.DataContext = new AnotherMVVMDemo.StudentViewModel(); // this is more apropriate if StudentViewModel had parameters in the constructor.
+
+        }
+    }
+}
